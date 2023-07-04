fd07bb16df096b4128253632bd6e6bbcdc118cf7
Marc Ablay <marc.ablay@gmail.com>
2023-07-04 23:25:44 +0200

    Updates

---
+++ nuxt/index.d2b9dc60.js
@@ -0,0 +1,21 @@
+import{_ as c}from"./nuxt-link.dc2e43e3.js";import{_ as d,c as i,a as e,b as t,w as a,o as _,p as r,e as l}from"./entry.be18ee89.js";const h={},o=s=>(r("data-v-1d50b421"),s=s(),l(),s),p={class:"page"},u={class:"container"},f=o(()=>e("h1",null,"Sky Browser",-1)),m={class:"row"},v={class:"col"},x=o(()=>e("div",{class:"card"},"Images",-1)),b={class:"col"},g=o(()=>e("div",{class:"card"},"Meshes",-1)),k=o(()=>e("div",{class:"about"},[e("pre",null,`Status: Most meshes and textures can be displayed
+
+Credits to:
+* TGC for their wonderful work
+* Lukas & Chara for RE insights
+* longbyte1 for the mesh decode script
+* DancingTwix, Alturus, LivingMaize
+  for infos in Discord and Forums
+* Gred Zaal & Jarod Guest from polyhaven.com
+  for their 3D sky background image
+
+Relevant JS libraries:
+* three
+* lz4js
+* @zip.js/zip.js
+* vue3
+* nuxt3
+
+texgenpack has been compiled to WASM, so
+your browser can load ETC2 KTX files.
+        `)],-1));function w(s,y){const n=c;return _(),i("div",p,[e("div",u,[f,e("div",m,[e("div",v,[t(n,{to:"ktx"},{default:a(()=>[x]),_:1})]),e("div",b,[t(n,{to:"mesh"},{default:a(()=>[g]),_:1})])]),k])])}const I=d(h,[["render",w],["__scopeId","data-v-1d50b421"]]);export{I as default};
